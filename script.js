27af5e757eb7aaa2936d59c4a91477b04e11980f
abdulziz12 <abdurashidov200930@gmail.com>
2024-07-04 10:34:19 +0500

    qwdazefadsd
    adacxsdc

---
+++ script.js
@@ -0,0 +1,69 @@
+let myArray = [1 , 2, 3, 4];
+
+
+console.log("Array uzunligi: " + myArray.length);
+alert("Array uzunligi: " + myArray.length);
+
+
+let userResponse = confirm("Elementni o'chirishni xohlaysizmi?");
+
+
+if (userResponse) {
+    myArray.pop();
+    console.log("Yangi array uzunligi: " + myArray.length);
+    alert("Yangi array uzunligi: " + myArray.length);
+} else {
+    console.log("Array o'zgartirilmagan");
+    alert("Array o'zgartirilmagan");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr=['banan,nok ,lma']
+// let array = alert("Boshlanishidan arrayning uzunligi :4")
+// let arra = alert("sizning arrayingizda malunotlarni olib tashlamoqchimiz")
+
+
+// array.push('olma,nok,banan,shoptol')
+// console.log(array);
+
+
+
+// console.log(mashina.split(" "))
+
+// mashinalar.push(mashina, mashina_1, mashina_2)
+// console.log(mashinalar);
+
+// if (mashinalar.(klient)){
+//     alert(`${klient} bor `)
+// }else{
+//     alert("mashina salonda yoq")
+// }
